b213800e2e6aebebfd09f28dfd10b1a322aca326
JFronny <git@akbvopenfl1.hopto.org>
2024-08-09 17:08:53 +0200

    build: compute version code and name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,34 @@ plugins {
     id("org.jetbrains.kotlin.plugin.compose")
 }
 
+val versionMajor = 1
+val versionMinor = 0
+val versionPatch = 0
+val versionBuild = System.getenv("CI_PIPELINE_IID")?.toIntOrNull() ?: 0
+
+val computedVersionName by lazy { "$versionMajor.$versionMinor.$versionPatch" + if (versionBuild > 0) "+$versionBuild" else "" }
+
+// Version code: S VVVVV MMMMMMM PPPPP IIIIIIIIIIIIII (32-bit integer)
+// S (x1):  Sign bit. Must always be 0 for an android version code
+// V (x5):  Major version. Up to 32, which should be enough (especially since we are still on 0)
+//          This might not work for apps that follow proper semantic versioning, but who does that?
+// M (x7):  Minor version. Up to 128, which should be enough
+// P (x5):  Patch version. Up to 32, which should be enough for these
+// I (x14): Pipeline ID bits. Allows a total of 16384 pipeline runs.
+//          I'm simply guessing that that'll be enough
+//
+// This implementation assumes that these maximum numbers will never be reached.
+// If they are reached, the version codes "bleed over" into the next range,
+// so this should technically still produce valid, higher versions, but the format will be broken.
+val computedVersionCode by lazy {
+    var bits = 0
+    bits = (bits shl 5) or versionMajor
+    bits = (bits shl 7) or versionMinor
+    bits = (bits shl 5) or versionPatch
+    bits = (bits shl 14) or versionBuild
+    bits
+}
+
 android {
     namespace = "io.gitlab.jfronny.kirchenfuehrung.client"
     compileSdk = 34
@@ -14,8 +42,8 @@ android {
         applicationId = "io.gitlab.jfronny.kirchenfuehrung.client"
         minSdk = 22
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = computedVersionCode
+        versionName = computedVersionName
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
