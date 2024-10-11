8c889081d8e5dcc23382cb20faa459cadfbb4c5c
JFronny <git@akbvopenfl1.hopto.org>
2024-10-11 17:29:31 +0200

    fix: host data on GitHub
    this drastically improves performance on our WiFi for some reason

--- app/src/main/java/de/kirchewds/kirchenfuehrung/client/ClientApplication.kt
+++ app/src/main/java/de/kirchewds/kirchenfuehrung/client/ClientApplication.kt
@@ -7,6 +7,6 @@ import dagger.hilt.android.HiltAndroidApp
 class ClientApplication: Application() {
     companion object {
         const val TOUR_URI = "kirchenfuehrung://"
-        const val TOURS_JSON_URI = "https://kirchewds.gitlab.io/kirchenfuehrung-data/tours.json"
+        const val TOURS_JSON_URI = "https://kirchewds.github.io/kirchenfuehrung-data/tours.json"
     }
 }
