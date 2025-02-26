9306e1bc0740a7177c25c20ec52d93459cabe6b1
JFronny <git@akbvopenfl1.hopto.org>
2025-02-26 19:48:53 +0100

    chore: update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,12 +36,12 @@ val computedVersionCode by lazy {
 
 android {
     namespace = "de.kirchewds.kirchenfuehrung.client"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "de.kirchewds.kirchenfuehrung.client"
         minSdk = 22
-        targetSdk = 34
+        targetSdk = 35
         versionCode = computedVersionCode
         versionName = computedVersionName
 
@@ -72,9 +72,6 @@ android {
     buildFeatures {
         compose = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.15"
-    }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -84,43 +81,41 @@ android {
 
 dependencies {
     // JDK support
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs_nio:2.1.2")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs_nio:2.1.5")
     // API
     implementation("io.gitlab.jfronny:gson:2.10.3-SNAPSHOT")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     // Images
     implementation("io.coil-kt:coil-compose:2.6.0")
     // Audio
-    implementation("androidx.media3:media3-exoplayer:1.4.1")
-    implementation("androidx.media3:media3-session:1.4.1")
-    implementation("androidx.media3:media3-datasource-okhttp:1.4.1")
-    implementation("androidx.media3:media3-exoplayer-workmanager:1.4.1")
+    implementation("androidx.media3:media3-exoplayer:1.5.1")
+    implementation("androidx.media3:media3-session:1.5.1")
+    implementation("androidx.media3:media3-datasource-okhttp:1.5.1")
+    implementation("androidx.media3:media3-exoplayer-workmanager:1.5.1")
     // Compose
-    implementation(platform("androidx.compose:compose-bom:2024.09.03"))
+    implementation(platform("androidx.compose:compose-bom:2025.02.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.material3:material3-window-size-class")
-    implementation("androidx.navigation:navigation-compose:2.8.2")
-    implementation("androidx.activity:activity-compose:1.9.2")
+    implementation("androidx.navigation:navigation-compose:2.8.8")
+    implementation("androidx.activity:activity-compose:1.10.1")
     // Hilt
-    implementation("com.google.dagger:hilt-android:2.52")
-    kapt("com.google.dagger:hilt-android-compiler:2.52")
+    implementation("com.google.dagger:hilt-android:2.55")
+    kapt("com.google.dagger:hilt-android-compiler:2.55")
     // Other
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.9.24"))
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.6")
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom:2.1.0"))
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-guava:1.8.1")
     // Tests
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.09.03"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
